a8144454a176d9ec6c65e7f5d585e21eb40df2bb
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2022-06-10 11:04:56 +0800

    done get auth me

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\V1\Auth\LoginController;
+use App\Http\Resources\User\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +22,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/v1/auth/logout', [LoginController::class, 'logout'])->name('auth.logout');
+    Route::get('/v1/auth/me', function (Request $request) {
+        return new UserResource($request->user());
+    });
 });
 
 Route::post('/v1/auth/login', [LoginController::class, 'login'])->name('auth.login');
