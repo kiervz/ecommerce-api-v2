b6269acaec76204b02a76201e1370f7fdc45d1e9
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2022-06-12 00:50:28 +0800

    add category model, controller and api route

---
+++ database/migrations/2022_06_11_164329_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Segment;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreignIdFor(User::class, 'user_id');
+            $table->foreignIdFor(Segment::class, 'segment_id');
+            $table->string('name');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
