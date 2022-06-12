45d9534df32302da326c2d69f5b126a4971e446f
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2022-06-12 12:02:02 +0800

    add sub-category model, controller and api route

---
+++ database/migrations/2022_06_12_035642_create_sub_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreignIdFor(User::class, 'user_id');
+            $table->foreignIdFor(Category::class, 'category_id');
+            $table->string('name');
+            $table->string('slug');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_categories');
+    }
+}
