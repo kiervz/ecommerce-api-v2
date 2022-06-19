d23558ee69d6fc115bc8e03a338378b36d39cd20
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2022-06-19 21:04:24 +0800

    add store model, controller and api route

---
+++ app/Models/Store.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Cviebrock\EloquentSluggable\Sluggable;
+
+class Store extends Model
+{
+    use HasFactory, SoftDeletes, Sluggable;
+
+    protected $fillable = [
+        'seller_id',
+        'name',
+        'slug',
+        'bio',
+        'last_log',
+    ];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
+}
---
+++ database/migrations/2022_06_19_125753_create_stores_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Seller;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreignIdFor(Seller::class, 'seller_id');
+            $table->string('name', 191);
+            $table->string('slug', 191);
+            $table->longText('bio');
+            $table->timestamp('last_log')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
